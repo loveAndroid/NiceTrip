bac0aba7a0dda8a60c7abc41971980c524ad905a
kblog <502902794@.com>
2016-08-21 17:14:46 +0800

    add hot fix file

---
+++ hotFix/Android-hot-fix/src/hot/Test.java
@@ -0,0 +1,17 @@
+package hot;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 测试类 
+ * @author alex_mahao
+ *
+ */
+public class Test {
+
+	public static void show(Context context){
+		Toast.makeText(context, "1", Toast.LENGTH_SHORT).show();
+	}
+	
+}
